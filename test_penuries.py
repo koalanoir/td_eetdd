bd9f22ecaf93528e0499b5aee940e0e69ce6153d
koalanoir <ngdorian0@gmail.com>
2023-01-02 16:16:55 +0100

    test de penueries

--- test_penuries.py
+++ test_penuries.py
@@ -1,11 +1,8 @@
 from main import *
 import unittest
 class MachineACafeTest(unittest.TestCase):
-    """ 
-        failed
-    """
-    def Test_Sans_Eau(self):
-        #ETANT DONNE une machine
+    def test_sans_eau(self):
+               #ETANT DONNE une machine
         machine = Machine()
         machine.CouperEau()
         nombreCaféInitiaux = machine.GetNombreCafésServis()
@@ -23,34 +20,8 @@ class MachineACafeTest(unittest.TestCase):
         argentEncaisséFinal = machine.GetArgentEncaissé()
         self.assertEqual(argentEncaisséInitial + sommeInsérée, argentEncaisséFinal)
 
-
-    """ 
-        failed
-    """
-    def Test_Sans_Cafe(self):
-        #ETANT DONNE une machine
-        machine = Machine()
-        machine.CouperCafe()
-        nombreCaféInitiaux = machine.GetNombreCafésServis()
-        argentEncaisséInitial = machine.GetArgentEncaissé()
-        sommeInsérée = 0.40
-        
-        # QUAND on met 40cts
-        machine.Insérer(sommeInsérée)
-
-        #ALORS un café coule
-        nombreCafésFinaux = machine.GetNombreCafésServis()
-        self.assertEqual(nombreCaféInitiaux + 1, nombreCafésFinaux)
-
-        #ET l'argent est encaissé
-        argentEncaisséFinal = machine.GetArgentEncaissé()
-        self.assertEqual(argentEncaisséInitial + sommeInsérée, argentEncaisséFinal)
-
-    """ 
-        failed
-    """
-    def test_pas_d_argent(self):
-        #ETANT DONNE une machine
+    def test_sans_cafe(self):
+               #ETANT DONNE une machine
         machine = Machine()
         machine.PlusDeGobelet()
         nombreCaféInitiaux = machine.GetNombreCafésServis()
@@ -68,6 +39,24 @@ class MachineACafeTest(unittest.TestCase):
         argentEncaisséFinal = machine.GetArgentEncaissé()
         self.assertEqual(argentEncaisséInitial + sommeInsérée, argentEncaisséFinal)
 
+    def test_sans_gobelet(self):
+        #ETANT DONNE une machine
+        machine = Machine()
+        machine.PlusDeGobelet()
+        nombreCaféInitiaux = machine.GetNombreCafésServis()
+        argentEncaisséInitial = machine.GetArgentEncaissé()
+        sommeInsérée = 0.40
+        
+        # QUAND on met 40cts
+        machine.Insérer(sommeInsérée)
+
+        #ALORS un café coule
+        nombreCafésFinaux = machine.GetNombreCafésServis()
+        self.assertEqual(nombreCaféInitiaux + 1, nombreCafésFinaux)
+
+        #ET l'argent est encaissé
+        argentEncaisséFinal = machine.GetArgentEncaissé()
+        self.assertEqual(argentEncaisséInitial + sommeInsérée, argentEncaisséFinal)
 
 if __name__ == '__main__':
     unittest.main()
